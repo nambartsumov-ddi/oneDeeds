1093bfde70163d625319ec1cf5acde621dff6400
Yotam Elkaslasy <yotamelkaslasy@gmail.com>
2018-08-24 13:10:54 +0300

    upadted container app with a floder and style module

--- client/src/containers/App/index.js
+++ client/src/containers/App/index.js
@@ -4,10 +4,12 @@ import PropTypes from 'prop-types';
 import { hot } from 'react-hot-loader';
 
 // App routes
-import Routes from '../routes';
+import Routes from 'Routes';
+
+import styles from './App.module.scss';
 
 const App = ({ history }) => (
-  <div className="app">
+  <div className={styles.AppContainer}>
     <ConnectedRouter history={history}>
       <Routes />
     </ConnectedRouter>
