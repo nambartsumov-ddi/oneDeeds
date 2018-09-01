206b3e957e5224335e993bf85e2624bb469cfa28
Tal Sasson <tal.sason1@gmail.com>
2018-09-01 18:25:59 +0300

    Added design for layout panel component (title, description, background image)

--- src/client/components/Layout/index.js
+++ src/client/components/Layout/index.js
@@ -6,16 +6,35 @@ import PropTypes from 'prop-types';
 
 import styles from './Layout.module.scss';
 
+import ArtBeach from 'Images/art-beach.jpg';
+import AgedBaby from 'Images/adult-aged-baby.jpg';
+import Adult from 'Images/abstract-adult.jpg';
+
 class Layout extends Component {
   render() {
     return (
       <div className={styles.Layout + ' ' + (this.props.isOpen ? styles.navOpen : '')}>
-        <LayoutPanel side={styles.left} />
+        <LayoutPanel
+          side={styles.left + ' ' + styles.Panel}
+          title="Section 1"
+          description="Description for section 1"
+          image={ArtBeach}
+        />
         <LayoutPanel side={styles.right}>
-          <LayoutPanel side={styles.rightHalfTop}>
+          <LayoutPanel
+            side={styles.rightHalfTop + ' ' + styles.Panel}
+            title="Section 2"
+            description="Description for section 2"
+            image={AgedBaby}>
             <LayoutButton text="Act Now" />
           </LayoutPanel>
-          <LayoutPanel side={styles.rightBottom} />
+          <LayoutPanel
+            side={styles.rightBottom + ' ' + styles.Panel}
+            title="Section 3"
+            description="Description for section 3"
+            image={Adult}>
+            <LayoutButton text="Get Started" />
+          </LayoutPanel>
         </LayoutPanel>
       </div>
     );
