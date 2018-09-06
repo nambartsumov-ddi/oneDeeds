d649bc83899e07b6bb58762b8741d72aa92875b6
Yotam Elkaslasy <yotamelkaslasy@gmail.com>
2018-09-06 20:53:02 +0300

    nav toggle refactor and animation

--- src/client/components/Nav/index.js
+++ src/client/components/Nav/index.js
@@ -1,32 +1,38 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import styles from './Nav.module.scss';
 
-class Nav extends Component {
-  render() {
-    let styleLink = this.props.isOpen ? { opacity: '1', marginLeft: '0' } : {};
+// Because we use css-modules we need to bind styles to classNames utilitie
+const stylesCtx = classNames.bind(styles);
 
-    return (
-      <div className={styles.Nav + ' ' + (this.props.isOpen ? styles.navOpen : '')}>
-        {/* <NavMenu /> */}
-        <div className={styles.mainLinks}>
-          {/* NavMenuItem */}
-          <div className={styles.link} style={styleLink}>
-            First Page
-          </div>
-          <div className={styles.link} style={styleLink}>
-            Second Page
-          </div>
-          <div className={styles.link} style={styleLink}>
-            Third Page
-          </div>
+const Nav = ({ isOpen }) => {
+  const styleLink = isOpen ? { opacity: '1', marginLeft: '0' } : {};
+
+  const NavClasses = stylesCtx(styles.Nav, {
+    [styles.navOpen]: isOpen,
+  });
+
+  return (
+    <div className={NavClasses}>
+      {/* TODO: Add <NavMenu /> */}
+      <div className={styles.mainLinks}>
+        {/* TODO: Add <NavMenuItem /> */}
+        <div className={styles.link} style={styleLink}>
+          First Page
+        </div>
+        <div className={styles.link} style={styleLink}>
+          Second Page
+        </div>
+        <div className={styles.link} style={styleLink}>
+          Third Page
         </div>
       </div>
-    );
-  }
-}
+    </div>
+  );
+};
 
 Nav.propTypes = {
   isOpen: PropTypes.bool,
