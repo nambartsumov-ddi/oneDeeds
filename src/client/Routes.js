09479adee8a7c7f73a51e7525c564d00a6280fc4
Yotam Elkaslasy <yotamelkaslasy@gmail.com>
2018-09-13 16:24:12 +0300

    client: Removed Router as it's not needed

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Home from 'Pages/Home';
 import About from 'Pages/About';
@@ -8,13 +8,11 @@ import Nav from 'Components/Nav';
 const Routes = () => (
   <>
     <Nav />
-    <Router>
-      <Switch>
-        <Route path="/" exact component={Home} />
-        <Route path="/about-the-cause" exact component={About} />
-        <Redirect to="/" />
-      </Switch>
-    </Router>
+    <Switch>
+      <Route path="/" exact component={Home} />
+      <Route path="/about-the-cause" exact component={About} />
+      <Redirect to="/" />
+    </Switch>
   </>
 );
 
