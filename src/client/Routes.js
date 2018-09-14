ece41b5c5131556d152af56d5eb490caa582e26d
Tal Sasson <tal.sason1@gmail.com>
2018-09-14 11:37:37 +0300

    Added overlay

--- src/client/Routes.js
+++ src/client/Routes.js
@@ -4,10 +4,12 @@ import { Route, Switch, Redirect } from 'react-router-dom';
 import Home from 'Pages/Home';
 import About from 'Pages/About';
 import Nav from 'Components/Nav';
+import Overlay from 'Components/Overlay';
 
 const Routes = () => (
   <>
     <Nav />
+    <Overlay />
     <Switch>
       <Route path="/" exact component={Home} />
       <Route path="/about-the-cause" exact component={About} />
