804e136de4c1699da835d75663c041ae698dbf49
Tal Sasson <tal.sason1@gmail.com>
2018-09-29 13:41:56 +0300

    Client: continue design registration component

--- src/client/components/Registration/index.js
+++ src/client/components/Registration/index.js
@@ -1,21 +1,34 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import Steps from 'Components/Steps';
 import Email from 'Components/Email';
+import NavToggle from 'Components/NavToggle';
+import Logo from 'Components/Logo';
 
 import styles from './Registration.module.scss';
 
+// Because we use css-modules we need to bind styles to classNames utilities
+const stylesCtx = classNames.bind(styles);
+
 const Registration = () => {
+  const facebookClasses = stylesCtx(styles.Login, styles.Facebook);
+  const googleClasses = stylesCtx(styles.Login, styles.Google);
+
   return (
     <div className={styles.Registration}>
-      <h1 className={styles.Title}>Registration</h1>
+      <NavToggle page="registration" />
+      <Logo />
       <Steps />
       <Email />
-      <span className={styles.Or}>Or</span>
+      <span className={styles.Or}>OR</span>
       <div className={styles.SocialBtnWrapper}>
-        <a href="/auth/facebook">Facebook</a>
-        <a href="/auth/google">Google</a>
-        <a href="/auth/instagram">Instagram</a>
+        <a href="/auth/facebook" className={facebookClasses}>
+          Login with Facebook
+        </a>
+        <a href="/auth/google" className={googleClasses}>
+          Login with Google
+        </a>
       </div>
     </div>
   );
