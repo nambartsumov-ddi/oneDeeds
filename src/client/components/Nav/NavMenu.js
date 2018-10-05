cddcd44e65e617bebc8ceac2c1191e8ba90f8931
Yotam Elkaslasy <yotamelkaslasy@gmail.com>
2018-10-06 02:18:52 +0300

    client: posed animations added to nav and refactor

--- src/client/components/Nav/NavMenu.js
+++ src/client/components/Nav/NavMenu.js
@@ -1,5 +1,8 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import posed from 'react-pose';
 
 import styles from './NavMenu.module.scss';
 import NavMenuLink from './NavMenuLink';
@@ -7,19 +10,68 @@ import NavMenuLink from './NavMenuLink';
 // Because we use css-modules we need to bind styles to classNames utilities
 const stylesCtx = classNames.bind(styles);
 
-const NavMenu = () => {
+const PosedItems = posed.ul({
+  open: {
+    initialPose: 'closed',
+    staggerChildren: 100,
+  },
+});
+
+const PosedItem = posed.li({
+  initialPose: 'closed',
+  open: {
+    x: '0%',
+    opacity: 1,
+    transition: {
+      type: 'spring',
+      stiffness: '100',
+    },
+  },
+  closed: {
+    x: '3rem',
+    opacity: 0,
+  },
+});
+
+const NavMenu = ({ isOpen }) => {
   const navMenuClasses = stylesCtx(styles.NavMenu);
+  const posedItemsClass = stylesCtx(styles.PosedItems);
+  const posedItemClass = stylesCtx(styles.PosedItem);
 
   return (
     <nav className={navMenuClasses}>
-      <NavMenuLink title="Home" to={`/`} />
-      <NavMenuLink title="Activity" to={`/activity`} />
-      <NavMenuLink title="Our Community" to={`/our-community`} />
-      <NavMenuLink title="About The Cause" to={`/about-the-cause`} />
-      <NavMenuLink title="Donations" to={`/Donations`} />
-      <NavMenuLink title="FAQ" to={`/frequently-asked-questions`} />
+      <PosedItems className={posedItemsClass} pose={isOpen ? 'open' : 'closed'}>
+        <PosedItem className={posedItemClass}>
+          <NavMenuLink title="Home" to={`/`} />
+        </PosedItem>
+        <PosedItem className={posedItemClass}>
+          <NavMenuLink title="Activity" to={`/activity`} />
+        </PosedItem>
+        <PosedItem className={posedItemClass}>
+          <NavMenuLink title="Our Community" to={`/our-community`} />
+        </PosedItem>
+        <PosedItem className={posedItemClass}>
+          <NavMenuLink title="About The Cause" to={`/about-the-cause`} />
+        </PosedItem>
+        <PosedItem className={posedItemClass}>
+          <NavMenuLink title="Donations" to={`/Donations`} />
+        </PosedItem>
+        <PosedItem className={posedItemClass}>
+          <NavMenuLink title="FAQ" to={`/frequently-asked-questions`} />
+        </PosedItem>
+      </PosedItems>
     </nav>
   );
 };
 
-export default NavMenu;
+NavMenu.propTypes = {
+  isOpen: PropTypes.bool,
+};
+
+const mapStateToProps = (state) => {
+  return {
+    isOpen: state.ui.isNavOpen,
+  };
+};
+
+export default connect(mapStateToProps)(NavMenu);
