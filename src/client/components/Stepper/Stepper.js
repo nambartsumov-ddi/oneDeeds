44e3ba2ab04e851db1824df3c604fb6b75e893cb
Tal Sasson <tal.sason1@gmail.com>
2018-10-30 14:53:57 +0200

    client: stepper animation canceled

--- src/client/components/Stepper/Stepper.js
+++ src/client/components/Stepper/Stepper.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React from 'react';
 import classNames from 'classnames';
 import { PropTypes } from 'prop-types';
 
@@ -9,90 +9,31 @@ import svgIconSuccess from 'Assets/icons/green-succes.svg';
 // Because we use css-modules we need to bind styles to classNames utilities
 const stylesCtx = classNames.bind(styles);
 
-export default class Stepper extends Component {
-  constructor(props) {
-    super(props);
+const Stepper = ({ activeStep, steps }) => {
+  const RootClasses = stylesCtx(styles.Root);
+  const StepperClasess = stylesCtx(styles.Stepper);
 
-    this.mountStyle = this.mountStyle.bind(this);
-
-    this.state = {
-      style: {
-        opacity: 0,
-        // position: 'relative',
-        // top: '-100px',
-        transform: 'scaleX(0)',
-        transition: 'all 2s ease',
-      },
-    };
-  }
-
-  mountStyle() {
-    this.setState({
-      style: {
-        opacity: 1,
-        // position: 'relative',
-        // top: '0px',
-        transition: 'all 0.5s ease',
-      },
-    });
-  }
-
-  componentDidMount() {
-    setTimeout(this.mountStyle, 10);
-  }
-
-  render() {
-    const RootClasses = stylesCtx(styles.Root);
-    const StepperClasess = stylesCtx(styles.Stepper);
-    const { activeStep, steps } = this.props;
-
-    return (
-      <div className={RootClasses} style={this.state.style}>
-        <div className={StepperClasess}>
-          {steps.map((step, index) => (
-            <Step
-              key={index}
-              completedIcon={svgIconSuccess}
-              title={step.title}
-              completedTitle={step.completedTitle}
-              index={index}
-              isActive={index === activeStep}
-              isCompleted={index < activeStep}
-              isDefault={!(index === activeStep || index < activeStep)}
-              isFirst={index === 0}
-              isLast={index === steps.length - 1}
-            />
-          ))}
-        </div>
+  return (
+    <div className={RootClasses}>
+      <div className={StepperClasess}>
+        {steps.map((step, index) => (
+          <Step
+            key={index}
+            completedIcon={svgIconSuccess}
+            title={step.title}
+            completedTitle={step.completedTitle}
+            index={index}
+            isActive={index === activeStep}
+            isCompleted={index < activeStep}
+            isDefault={!(index === activeStep || index < activeStep)}
+            isFirst={index === 0}
+            isLast={index === steps.length - 1}
+          />
+        ))}
       </div>
-    );
-  }
-}
-// const Stepper = ({ activeStep, steps }) => {
-//   const RootClasses = stylesCtx(styles.Root);
-//   const StepperClasess = stylesCtx(styles.Stepper);
-
-//   return (
-//     <div className={RootClasses}>
-//       <div className={StepperClasess}>
-//         {steps.map((step, index) => (
-//           <Step
-//             key={index}
-//             completedIcon={svgIconSuccess}
-//             title={step.title}
-//             completedTitle={step.completedTitle}
-//             index={index}
-//             isActive={index === activeStep}
-//             isCompleted={index < activeStep}
-//             isDefault={!(index === activeStep || index < activeStep)}
-//             isFirst={index === 0}
-//             isLast={index === steps.length - 1}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
+    </div>
+  );
+};
 
 Stepper.defaultProps = {
   activeStep: 0,
@@ -103,4 +44,4 @@ Stepper.propTypes = {
   steps: PropTypes.array,
 };
 
-// export default Stepper;
+export default Stepper;
