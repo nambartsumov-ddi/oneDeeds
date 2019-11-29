33591303031bdf99d388395897c3b893d6a5e87f
Nikolay Ambartsumov <ambartsumov.nikolay@gmail.com>
2019-11-29 14:44:30 +0200

    Restore home page

--- src/client/components/Layout/Panel.js
+++ src/client/components/Layout/Panel.js
@@ -9,21 +9,14 @@ import styles from './Panel.module.scss';
 const stylesCtx = classNames.bind(styles);
 
 let i = 1;
-const Panel = ({ size, title, description, imageSrc, id, goTo, history, className, children, style, type }) => {
+const Panel = ({ size, title, description, imageSrc, id, goTo, history, className, children, style }) => {
   const panelClasses = stylesCtx(styles.Panel, {
     [styles.Full]: size === 'Full',
     [styles.Half]: size === 'Half',
-    [styles.Third]: size === 'Third',
-    [styles.TwoThirds]: size === 'TwoThirds',
-    [styles.CharityPanel]: type === 'Charity',
     [styles.WithDesc]: description !== undefined,
   });
 
   const imgPanelClasses = stylesCtx(styles.ImgPanel, {
-    [className]: className,
-  });
-
-  const fullImgPanelClasses = stylesCtx(styles.ImgPanelFull, {
     [className]: className,
   });
 
@@ -62,7 +55,7 @@ const Panel = ({ size, title, description, imageSrc, id, goTo, history, classNam
       onClick={() => onClick(goTo)}
       style={isMobile() ? null : style}>
       <div
-        className={size === 'Full' ? fullImgPanelClasses : imgPanelClasses}
+        className={imgPanelClasses}
         style={{
           backgroundImage: imageSrc ? `url(${imageSrc})` : null,
         }}>
@@ -84,7 +77,6 @@ Panel.propTypes = {
   className: PropTypes.string,
   children: PropTypes.any,
   style: PropTypes.object,
-  type: PropTypes.string,
 };
 
 export default withRouter(Panel);
--- src/client/pages/Home/index.js
+++ src/client/pages/Home/index.js
@@ -15,10 +15,6 @@ import handsFull from 'Images/hands-full2.jpg';
 import handHalf from 'Images/hands-half2.jpg';
 
 class Home extends Component {
-  isMobile() {
-    window.screen.width < 992;
-  }
-
   constructor(props) {
     super(props);
 
@@ -42,11 +38,11 @@ class Home extends Component {
   mountStyle() {
     this.setState({
       styleFullPanel: {
-        width: this.isMobile() ? '200vw' : '100vw',
+        width: '60vw',
         transition: 'width 2s ease',
       },
       styleHalfPanel: {
-        width: this.isMobile() ? '200vw' : '100vw',
+        width: '40vw',
         visibility: 'visible',
         position: 'relative',
         transition: 'width .3s ease',
@@ -63,6 +59,7 @@ class Home extends Component {
         position: 'absolute',
       },
     });
+
     setTimeout(this.mountHalfStyle, 300);
   }
 
@@ -104,12 +101,14 @@ class Home extends Component {
       window.requestAnimationFrame(step);
     };
 
+    const isMobile = () => window.screen.width < 992;
+
     // Because we use css-modules we need to bind styles to classNames utilities
     const stylesCtx = classNames.bind(styles);
 
     const imagePosition = stylesCtx(
       {
-        [styles.PanelImagePosition]: this.isMobile(),
+        [styles.PanelImagePosition]: isMobile(),
       },
       styles.VideoPanel
     );
@@ -121,43 +120,33 @@ class Home extends Component {
         <DownArrow onClick={() => scrollToAnimated(this.SecondSectionRef.offsetTop, 600)} />
         {/** Animate panels with pose */}
         <Layout>
-          <Panel size="Half" style={styles.StyleHalfPanel}>
-            <Panel
-              id="1"
-              className={imagePosition}
-              goTo={'/video'}
-              size="Half"
-              style={this.state.styleHalfPanel}
-              title={
-                <div className={styles.TitleWrapper}>
-                  <div className={styles.FirstLine}>
-                    One is <span className={styles.Powerful}>Powerful, </span>
-                    Many are <span className={styles.Unstoppable}>Unstoppable</span>
-                  </div>
+          <Panel
+            id="1"
+            className={imagePosition}
+            goTo={'/video'}
+            size="Full"
+            title={
+              <div className={styles.TitleWrapper}>
+                <div className={styles.FirstLine}>
+                  One is <span className={styles.Powerful}>Powerful, </span>
                 </div>
-              }
-              imageSrc={handsFull}>
-              <svg width="62" height="62" viewBox="0 0 153 153" className={styles.PlayIcon}>
-                <path d="M98.3 73.4L65 52.2c-1.4-.9-3.2-.9-4.6-.1-1.5.8-2.4 2.3-2.4 4v40.7c0 1.6.9 3.1 2.3 3.9.7.4 1.5.6 2.3.6s1.6-.2 2.3-.6l33.3-19.5c1.4-.8 2.2-2.2 2.2-3.8.1-1.7-.7-3.1-2.1-4z" />
-                <path d="M146.9 50.9c-7.6-21.1-24.1-37.6-45.3-45.2C93.5 2.8 85 1.4 76.4 1.4c-8.7 0-17.2 1.5-25.4 4.4-21.1 7.5-37.5 24-45.1 45C3 59.1 1.5 67.6 1.5 76.4c0 8.6 1.4 17 4.3 25 7.5 21.3 24 37.9 45.3 45.6 8.1 2.9 16.7 4.4 25.4 4.4 8.6 0 17.1-1.5 25.2-4.3 21.3-7.7 37.9-24.2 45.4-45.6 2.8-8 4.3-16.4 4.3-25 0-8.9-1.5-17.5-4.5-25.6zm-7 47.9C133.2 118 118.2 133 99 139.9c-7.2 2.6-14.9 3.9-22.6 3.9-7.9 0-15.5-1.3-22.8-4-19.1-6.9-33.9-21.8-40.7-40.9C10.3 91.6 9 84.1 9 76.3c0-7.9 1.3-15.6 4-23 6.9-18.9 21.6-33.6 40.6-40.5 7.3-2.6 15-4 22.8-4s15.4 1.3 22.6 3.9c19.1 6.8 33.9 21.6 40.7 40.6 2.7 7.3 4 15.1 4 23 .1 7.8-1.2 15.3-3.8 22.5z" />
-              </svg>
-            </Panel>
-            <Panel
-              id="4"
-              style={this.state.styleHalfPanel}
-              size="Half"
-              title="JFCS &amp; OneDeeds"
-              description="We at OneDeeds are starting a new and exciting collaboration with JFCS. Our online community will donate food and we will send it directly to JFCS and help combat hunger."
-              goTo={'/jfcs'}
-              className={styles.CharityPanel}
-              type="Charity">
-              <LayoutButton text="For more information click Here" type="Charity" />
-            </Panel>
+                <div className={styles.SecondLine}>
+                  Many are <span className={styles.Unstoppable}>Unstoppable</span>
+                </div>
+              </div>
+            }
+            imageSrc={handsFull}
+            style={this.state.styleFullPanel}>
+            <svg width="62" height="62" viewBox="0 0 153 153" className={styles.PlayIcon}>
+              <path d="M98.3 73.4L65 52.2c-1.4-.9-3.2-.9-4.6-.1-1.5.8-2.4 2.3-2.4 4v40.7c0 1.6.9 3.1 2.3 3.9.7.4 1.5.6 2.3.6s1.6-.2 2.3-.6l33.3-19.5c1.4-.8 2.2-2.2 2.2-3.8.1-1.7-.7-3.1-2.1-4z" />
+              <path d="M146.9 50.9c-7.6-21.1-24.1-37.6-45.3-45.2C93.5 2.8 85 1.4 76.4 1.4c-8.7 0-17.2 1.5-25.4 4.4-21.1 7.5-37.5 24-45.1 45C3 59.1 1.5 67.6 1.5 76.4c0 8.6 1.4 17 4.3 25 7.5 21.3 24 37.9 45.3 45.6 8.1 2.9 16.7 4.4 25.4 4.4 8.6 0 17.1-1.5 25.2-4.3 21.3-7.7 37.9-24.2 45.4-45.6 2.8-8 4.3-16.4 4.3-25 0-8.9-1.5-17.5-4.5-25.6zm-7 47.9C133.2 118 118.2 133 99 139.9c-7.2 2.6-14.9 3.9-22.6 3.9-7.9 0-15.5-1.3-22.8-4-19.1-6.9-33.9-21.8-40.7-40.9C10.3 91.6 9 84.1 9 76.3c0-7.9 1.3-15.6 4-23 6.9-18.9 21.6-33.6 40.6-40.5 7.3-2.6 15-4 22.8-4s15.4 1.3 22.6 3.9c19.1 6.8 33.9 21.6 40.7 40.6 2.7 7.3 4 15.1 4 23 .1 7.8-1.2 15.3-3.8 22.5z" />
+            </svg>
           </Panel>
-          <Panel size="Half" style={styles.StyleHalfPanel}>
+
+          <Panel size="Full" style={this.state.styleHalfPanel}>
             <Panel
               id="2"
-              style={this.state.styleHalfPanel}
+              style={this.state.styleFirstHalfPanel}
               size="Half"
               title="Make a change"
               goTo={'/signup'}
@@ -166,9 +155,10 @@ class Home extends Component {
               className={styles.RegistrationPanel}>
               <LayoutButton text="Join OneDeeds Today" />
             </Panel>
+
             <Panel
               id="3"
-              style={this.state.styleHalfPanel}
+              style={this.state.styleBottomHalfPanel}
               size="Half"
               goTo={'/our-community'}
               title="How does it work?"
@@ -181,12 +171,13 @@ class Home extends Component {
                   </ul>
                 </div>
               }
+              /* imageSrc={handHalf}*/
               className={styles.OurCommunityPanel}>
               <LayoutButton text="Help Us Make A Change" />
             </Panel>
+            {/* ref div for scrolling */}
+            <div style={{ visibility: 'hidden' }} ref={(section) => (this.SecondSectionRef = section)} />
           </Panel>
-          {/* ref div for scrolling */}
-          <div style={{ visibility: 'hidden' }} ref={(section) => (this.SecondSectionRef = section)} />
         </Layout>
       </div>
     );
